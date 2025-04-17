f613a5cfd7ccc19c89785f93bd4c7a9c3f75c4cf
Joseph <joemenjr@gmail.com>
2025-04-17 17:11:05 -0400

    grouping

--- main.py
+++ main.py
@@ -8,19 +8,31 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Creating a group of objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    # Create player object
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
 
         screen.fill("black")
         dt = clock.tick(60) / 1000
+
+        # Obejcts
+        for object in drawable:
+            object.draw(screen)
         
-        player.update(dt)
-        player.draw(screen)
         # Screen is rendered
         pygame.display.flip()
         
