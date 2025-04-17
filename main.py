503c3f09e2ee09d09d020cf0d1dbf79e3eefe761
Joseph <joemenjr@gmail.com>
2025-04-17 16:39:39 -0400

    triangle logic

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-    game = True
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while game == True:
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, "Black")
+
+        screen.fill("black")
+        player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+        player.draw(screen)
         pygame.display.flip()
-    
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        dt = clock.tick(60) / 1000
+        
+
         
 if __name__ == "__main__":
     main()
