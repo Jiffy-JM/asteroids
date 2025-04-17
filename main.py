359df5ff4878e0b4aba9f13621773e1046fa9109
Joseph <joemenjr@gmail.com>
2025-04-17 16:51:28 -0400

    "ad" movement keys

--- main.py
+++ main.py
@@ -8,19 +8,21 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
 
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
-        player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
-        player.draw(screen)
-        pygame.display.flip()
         dt = clock.tick(60) / 1000
         
-
+        player.update(dt)
+        player.draw(screen)
+        # Screen is rendered
+        pygame.display.flip()
         
 if __name__ == "__main__":
     main()
